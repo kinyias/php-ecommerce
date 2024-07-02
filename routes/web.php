2ae23a2eed266981ff4db0230d9dd44d625c2b5c
accountgithub2003 <95201020+duongnhatthien@users.noreply.github.com>
2024-07-03 01:11:27 +0700

    Revert "Thiên"
    This reverts commit 5b15c5fbcad303abe940b0b71b5f04307524fb8f.

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
-use App\Http\Controllers\Auth\ProviderController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/auth/{provider}/redirect', [ProviderController::class, 'redirect']);
- 
-Route::get('/auth/{provider}/callback', [ProviderController::class,'callback']);
-Route::get('/dashboard', [ProviderController::class, 'callback'])->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    return view('home.index');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/admin', function () {
+    return view('admin.dashboard.index');
+});
